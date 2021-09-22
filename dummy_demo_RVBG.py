25122e93c36e191cb818d65828c76834497ead3b
Bruna Guedes <65077816+guedesbruna@users.noreply.github.com>
2021-09-22 07:42:20 +0200

    results for pop=50 gen=30

--- dummy_demo_RVBG.py
+++ dummy_demo_RVBG.py
@@ -62,7 +62,7 @@ n_vars = (env.get_num_sensors()+1)*n_hidden_neurons + (n_hidden_neurons+1)*5
 
 dom_u = 1
 dom_l = -1
-npop = 100 #100
+npop = 50 #100
 gens = 30 #30
 mutation = 0.2 #0.2
 last_best = 0
